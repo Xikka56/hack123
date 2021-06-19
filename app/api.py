bd09e6c871524451364a8c35e0a698526d6769c5
Xikka56 <70409596+Xikka56@users.noreply.github.com>
2021-06-19 11:16:02 +0300

    Го

--- app/api.py
+++ app/api.py
@@ -2,13 +2,43 @@ from app import app
 
 from service import JWT
 from app import *
+import pymysql
 
 from settings import env
 tokin_service = JWT.tokinService(env)
 
 
-# resp.set_cookie('sessionID', '', expires=0)   #Сбросить куки
+class DB():
+    """Работа с Базой данных
+    получить данные::
 
+        DB.GET('Текст запроса SQL')
+    отправить данные::
+
+        DB.POST('Текст запроса SQL')
+    """
+
+    def GET(self):
+        """Получает данные с Базы данных
+        """
+        connection = pymysql.connect(host="31.31.196.245", user='u0981115',
+                                     password='qwert5656', database='u0981115_hack123', charset="utf8mb4")
+        cursor = connection.cursor()
+        cursor.execute(self)
+        OTV = cursor.fetchall()
+        return(OTV)
+
+    def POST(self):
+        """Отправляет данные в Базу данных
+        """
+        connection = pymysql.connect(host="31.31.196.245", user='u0981115',
+                                     password='qwert5656', database='u0981115_hack123', charset="utf8")
+        cursor = connection.cursor()
+        cursor.execute(self)
+        connection.commit()
+        return('True')
+
+# resp.set_cookie('sessionID', '', expires=0)   #Сбросить куки
 
 
 @app.route('/api/v0.1/login', methods=['POST', "GET"])
@@ -17,16 +47,17 @@ def api_jwt_get():
         abort(405)
     if not request.cookies.get('refreshToken'):
         f = json.loads(request.data.decode())
-        jwts = tokin_service.generateTokins({"dq":'qwe'})
-        res = make_response({'accessToken' : jwts[0]})
+        jwts = tokin_service.generateTokins({"dq": 'qwe'})
+        res = make_response({'accessToken': jwts[0]})
         res.set_cookie('refreshToken', jwts[1], 60*24*30)
 
         # sql Обновить refreshToken в базе
 
     return res
 
+
 @app.route('/api/v0.1/users', methods=['POST', "GET"])
-def test():  
+def test():
     f = json.loads(request.data.decode())
     if('refreshToken' in f):
         jwts = tokin_service.decodeTokins('REFRESH', f['refreshToken'])
@@ -39,48 +70,54 @@ def test():
     return(jwts)
 
 
-
-
 db = {
-    0:{
-        'title':'Вечериночка',
-        'data':'12/07/2020',
-        'type':'ТУСА'
+    0: {
+        'title': 'Вечериночка',
+        'data': '12/07/2020',
+        'type': 'ТУСА'
     },
-    1:{
-        'title':'Футбик',
-        'data':'12/07/2020',
-        'type':'СПОРТ'
+    1: {
+        'title': 'Футбик',
+        'data': '12/07/2020',
+        'type': 'СПОРТ'
     },
-    2:{
-        'title':'Рыбалочка',
-        'data':'25/07/2020',
-        'type':'ОТДЫХ'
+    2: {
+        'title': 'Рыбалочка',
+        'data': '25/07/2020',
+        'type': 'ОТДЫХ'
     }
 }
 
-@app.route('/api/v0.1/get_block', methods=['POST', "GET"])
-def iget_blockdex():
+
+@app.route('/api/v0.1/get_slides', methods=['POST', "GET"])
+def get_slides():
     # asd
     if(request.data):
-        f = json.loads(request.data.decode())
-        print(f)
-        if('count' in f):
-            print(f['count'])
+        data = json.loads(request.data.decode())
+        print("data", data)
+        if(data["count"] != 0):
             m_end = []
-            q = 0
-            for el in range(f['count']):
-                try:
-                    m_end.append(db[q])
-                    q += 1
-                except:
-                    q = 0
-                    m_end.append(db[q])
-                    q += 1
-                # q += 1
-            print(len(m_end))
+            get_by_type = ""
+            if(data["type"]):
+                print(data["type"])
+                get_by_type = f"""WHERE`type` = '{data["type"]}' """
+
+            for el in range(data['count']):
+                m_end.append(
+                    DB.GET(f"""SELECT * FROM `slides` {get_by_type}""")[0])
         # jwts = tokin_service.generateTokins({"dq":'qwe'})
         return {
-            'mass_data':m_end
+            'mass_data': m_end
         }
     return('false')
+
+
+# @app.route('/api/v0.1/get_slides', methods=['POST'])
+# def get_slides():
+#     f = json.loads(request.data.decode())
+
+#     slide = DB.GET("""SELECT * FROM `slides`""")[0]
+#     print(slide)
+#     f = {slide[0]}
+#     print(f)
+#     return f, 200
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
-from app import app 
+from app import app
 
-app.run(host='localhost', debug = True)
+app.run(host='192.168.43.47', port=5000, debug=True)
